47499c35136671221c373469c6089608bd33a424
Sinitsa Egor Yuryevich <esinitsa@ozon.ru>
2024-05-15 16:37:37 +0300

    init ms

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+    "net/http"
+)
+
+// Define a counter metric
+var (
+    requestCount = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_total",
+            Help: "Number of HTTP requests received.",
+        },
+        []string{"path"},
+    )
+)
+
+func init() {
+    // Register the counter with Prometheus's default registry
+    prometheus.MustRegister(requestCount)
+}
+
+func main() {
+    r := gin.Default()
+
+    // Define the GET endpoint
+    r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+    r.GET("/your-endpoint", func(c *gin.Context) {
+        path := c.FullPath()
+        requestCount.WithLabelValues(path).Inc()
+        c.String(http.StatusOK, "Hello, World!")
+    })
+
+    // Run the server
+    r.Run(":8080") // Listen and serve on 0.0.0.0:8080
+}
+
